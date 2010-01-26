81330ba328aa6aa911a363ed5bdd75a198d50ad6
Daniel <daniel@daniel-desktop.(none)>
2010-01-27 10:26:15 +1100

    requirex for local files

---
+++ src/rubyxr.rb
@@ -0,0 +1,18 @@
+
+module Kernel
+  def requirex(file_path)
+    case file_path
+    when /^\.\//
+      puts "local file (#{file_path})"
+      require file_path.gsub(/^\./, Rubyxr.directory(caller[0]))
+    else
+      require file_path
+    end
+  end
+end
+
+module Rubyxr
+  def self.directory(stack_trace_entry)
+    File.dirname(stack_trace_entry.match(/(.*):\d/)[1])
+  end
+end
---
+++ test/local_file.rb
@@ -0,0 +1,2 @@
+class LocalFile
+end
