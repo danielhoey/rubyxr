0bd89ab3659756001c75164d511ec7c44725e20d
Daniel Hoey <danielhoey1@gmail.com>
2010-03-17 20:28:36 +1100

    test using rubyxr with regular ruby exe

--- test/binary.rb
+++ test/binary.rb
@@ -8,4 +8,10 @@ class Binary < Test::Unit::TestCase
     create_file('rubyxr_binary_test_file.rb', 'requirex "./print_requirex_works"')
     assert_equal('requirex works', `bin/rubyxr test/rubyxr_binary_test_file.rb`.strip)
   end
+  
+  def test_ruby_binary
+    create_file('print_requirex_works_with_ruby.rb', 'puts "requirex works"')
+    create_file('rubyxr_binary_test_file.rb', ["require '#{File.dirname(__FILE__)}/../src/rubyxr'", "requirex './print_requirex_works_with_ruby'"].join("\n"))
+    assert_equal('requirex works', `ruby test/rubyxr_binary_test_file.rb`.strip)
+  end
 end
